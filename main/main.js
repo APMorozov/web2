aaebb2f926e841053b0f85683c171a9791c97322
APMorozov <morozov-sasha200@yandex.ru>
2025-02-06 20:09:04 +0400

    lab-2

--- main/main.js
+++ main/main.js
@@ -1,18 +1,103 @@
 "use strict";
-let f1 = (elm1, elm2, elm3) => {
-    return elm1 + elm2 + elm3;
+Object.defineProperty(exports, "__esModule", { value: true });
+let array0 = [2, 4, 6];
+let array1 = [3, 5, 7];
+let interval = [3, 4, 5, 44];
+let f1 = (arr) => {
+    let sum = 0;
+    for (let num of arr) {
+        sum += num;
+    }
+    return sum;
 };
-console.log(f1(3, 5, 6));
-const age = 23;
-const surname = "Ivanov";
-let arr = [3, 5, 6];
-console.log(arr);
-let flag = true;
-console.log(flag);
-const data = {
-    id: 3,
-    e1: 12345,
-    e2: "Поле 2",
+console.log('1:');
+console.log('Array:', array0);
+console.log('Answer:');
+console.log(f1(array0));
+let array2 = [array0, array1];
+console.log(array2);
+console.log('interval:', interval);
+let f2 = (arr, interval) => {
+    let count = 0;
+    for (let num of interval) {
+        if (arr[0].includes(num)) {
+            count += 1;
+        }
+        if (arr[1].includes(num)) {
+            count += 1;
+        }
+    }
+    return count;
 };
-const jsonData = JSON.stringify(data);
-console.log(jsonData);
+console.log('Answer:');
+console.log(f2(array2, interval));
+console.log('2:');
+let tuple = ['Ivan', 'Ivanov', 52];
+let f3 = (tuple) => {
+    return tuple[0] + '-' + tuple[2] + '-' + tuple[1];
+};
+console.log('Tuple:', tuple);
+console.log('Answer:');
+console.log(f3(tuple));
+console.log('3:');
+var types_of_oil;
+(function (types_of_oil) {
+    types_of_oil["cocoa_butter"] = "Cocoa butter";
+    types_of_oil["sesame_oil"] = "Sesame oil";
+    types_of_oil["soybean_oil"] = "Soybean oil";
+})(types_of_oil || (types_of_oil = {}));
+;
+console.log('Answer:');
+console.log(types_of_oil.cocoa_butter);
+class Pet {
+    constructor() {
+        this.name = 'Some pet';
+        this.age = -1;
+    }
+    speak() {
+        return "No speak. I am fish!";
+    }
+}
+class Dog extends Pet {
+    constructor() {
+        super(...arguments);
+        this.label = "AngryHunter";
+        this.age = 8;
+    }
+    speak() {
+        return "Yaw-Gaw!";
+    }
+}
+class Cat extends Pet {
+    constructor() {
+        super(...arguments);
+        this.name = 'Barsik';
+        this.age = 2;
+    }
+    speak() {
+        return "Miyau!";
+    }
+}
+function printPetInfo(pet) {
+    console.log(`Name: ${pet.name}`);
+    console.log(`Age: ${pet.age}`);
+    console.log(`Speak: ${pet.speak()}`);
+    if (pet instanceof Dog) {
+        console.log(`Label: ${pet.label}`);
+    }
+}
+const myDog = new Dog();
+const myCat = new Cat();
+console.log('4:');
+console.log('Answer:');
+printPetInfo(myDog);
+printPetInfo(myCat);
+console.log('5:');
+let myOil = {
+    Proteins: 0,
+    Fats: 99,
+    Carbohydrates: 0,
+    type: types_of_oil.soybean_oil
+};
+console.log('Answer:');
+console.log(JSON.stringify(myOil));
--- main/main.ts
+++ main/main.ts
@@ -1,31 +1,110 @@
-let  f1 = (elm1:number, elm2:number, elm3:number):number => {
-    return elm1 + elm2 + elm3;
+import { TupleType } from "typescript";
+
+let array0: number[] = [2, 4, 6];
+let array1: number[] = [3, 5, 7];
+let interval: number[] = [3, 4, 5, 44];
+
+let f1 = (arr:number[]): number =>{
+    let sum:number = 0;
+    for(let num of arr){
+        sum += num;
+    }
+    return sum;
 }
-console.log(f1(3,5,6))
-
-const age:number = 23;
-const surname:string = "Ivanov";
-let arr:Array<number> = [3,5,6];
-console.log(arr);
-let flag:boolean = true;
-console.log(flag);
+console.log('1:');
+console.log('Array:',array0);
+console.log('Answer:');
+console.log(f1(array0));
 
 
-interface Entity {
-    id: number;
+let array2: number[][] = [array0, array1];
+console.log(array2);
+console.log('interval:', interval);
+
+let f2 = (arr:number[][], interval:number[]): number =>{
+    let count:number = 0;
+    for(let num of interval){
+        if(arr[0].includes(num)){
+            count+=1;
+        }
+        if(arr[1].includes(num)){
+            count+=1;
+        }
+    }
+    return count;
+}
+console.log('Answer:');
+console.log(f2(array2, interval))
+
+
+console.log('2:');
+let tuple: [string, string, number] = ['Ivan', 'Ivanov', 52];
+let f3 = (tuple:[string, string, number]): string =>{
+    return tuple[0] + '-' + tuple[2] + '-' + tuple[1];
+}
+console.log('Tuple:', tuple);
+console.log('Answer:');
+console.log(f3(tuple));
+
+console.log('3:');
+enum types_of_oil{cocoa_butter='Cocoa butter', sesame_oil='Sesame oil', soybean_oil='Soybean oil'};
+console.log('Answer:');
+console.log(types_of_oil.cocoa_butter);
+
+
+class Pet {
+    name: string = 'Some pet'
+    age: number = -1
+    speak() {
+    return "No speak. I am fish!";
+    }
+    }
+
+    class Dog extends Pet {
+        label = "AngryHunter";
+        age = 8;
+        speak() {
+        return "Yaw-Gaw!";
+        }
+        }
+        class Cat extends Pet {
+        name = 'Barsik';
+        age = 2;
+        speak() {
+        return "Miyau!";
+        }
+    }
+
+    function printPetInfo<T extends Pet>(pet: T): void {
+        console.log(`Name: ${pet.name}`);
+        console.log(`Age: ${pet.age}`);
+        console.log(`Speak: ${pet.speak()}`);
+        
+        if (pet instanceof Dog) {
+            console.log(`Label: ${pet.label}`);
+        }
+    }
+    
+    const myDog = new Dog();
+    const myCat = new Cat();
+    console.log('4:');
+    console.log('Answer:');
+    printPetInfo(myDog);
+    printPetInfo(myCat);
+
+console.log('5:');
+type PFC = {
+    Proteins: number;
+    Fats: number;
+    Carbohydrates: number;
+    type: types_of_oil;
 }
 
-interface ToJsonStringify extends
-Entity {
-e1?: number;
-e2: string;
+let myOil: PFC = {
+    Proteins: 0,
+    Fats: 99,
+    Carbohydrates: 0,
+    type: types_of_oil.soybean_oil
 }
-
-const data: ToJsonStringify = {
-id: 3,
-e1: 12345,
-e2: "Поле 2",
-}
-
-const jsonData: string = JSON.stringify(data);
-console.log(jsonData)
+console.log('Answer:');
+console.log(JSON.stringify(myOil));
