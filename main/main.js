4f39681278174c1ddd45d9025683dc85d18fe70f
APMorozov <morozov-sasha200@yandex.ru>
2025-02-06 17:45:08 +0400

    add main.js

---
+++ main/main.js
@@ -0,0 +1,18 @@
+"use strict";
+let f1 = (elm1, elm2, elm3) => {
+    return elm1 + elm2 + elm3;
+};
+console.log(f1(3, 5, 6));
+const age = 23;
+const surname = "Ivanov";
+let arr = [3, 5, 6];
+console.log(arr);
+let flag = true;
+console.log(flag);
+const data = {
+    id: 3,
+    e1: 12345,
+    e2: "Поле 2",
+};
+const jsonData = JSON.stringify(data);
+console.log(jsonData);
