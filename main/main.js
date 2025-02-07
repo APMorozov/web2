2cabd912310a1bff9804f73a4b7fee86c922d1aa
APMorozov <morozov-sasha200@yandex.ru>
2025-02-07 20:39:28 +0400

    lab3

--- main/main.js
+++ main/main.js
@@ -1,103 +1,93 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-let array0 = [2, 4, 6];
-let array1 = [3, 5, 7];
-let interval = [3, 4, 5, 44];
-let f1 = (arr) => {
-    let sum = 0;
-    for (let num of arr) {
-        sum += num;
+var docktype;
+(function (docktype) {
+    docktype["passpart"] = "\u041F\u0430\u0441\u0441\u043F\u043E\u0440\u0442";
+    docktype["snils"] = "\u0421\u041D\u0418\u041B\u0421";
+})(docktype || (docktype = {}));
+class Owner {
+    getSurname() {
+        return this._surname;
     }
-    return sum;
-};
-console.log('1:');
-console.log('Array:', array0);
-console.log('Answer:');
-console.log(f1(array0));
-let array2 = [array0, array1];
-console.log(array2);
-console.log('interval:', interval);
-let f2 = (arr, interval) => {
-    let count = 0;
-    for (let num of interval) {
-        if (arr[0].includes(num)) {
-            count += 1;
-        }
-        if (arr[1].includes(num)) {
-            count += 1;
-        }
+    getName() {
+        return this._name;
     }
-    return count;
-};
-console.log('Answer:');
-console.log(f2(array2, interval));
-console.log('2:');
-let tuple = ['Ivan', 'Ivanov', 52];
-let f3 = (tuple) => {
-    return tuple[0] + '-' + tuple[2] + '-' + tuple[1];
-};
-console.log('Tuple:', tuple);
-console.log('Answer:');
-console.log(f3(tuple));
-console.log('3:');
-var types_of_oil;
-(function (types_of_oil) {
-    types_of_oil["cocoa_butter"] = "Cocoa butter";
-    types_of_oil["sesame_oil"] = "Sesame oil";
-    types_of_oil["soybean_oil"] = "Soybean oil";
-})(types_of_oil || (types_of_oil = {}));
+    constructor(surname, name, subname, birthday, docktype, dock_series, dock_number) {
+        this._surname = surname;
+        this._name = name;
+        this._subname = subname;
+        this._birthday = birthday;
+        this._docktype = docktype;
+        this._dock_series = dock_series;
+        this._dock_number = dock_number;
+    }
+    getInfo() {
+        return 'Фамилия: ' + this._surname + ' Имя: ' + this._name + ' Отчество: ' + this._subname + ' Дата рождения: ' + this._birthday + ' Тип документа: ' + this._docktype + ' Серия: ' + this._dock_series + ' Номер: ' + this._dock_number;
+    }
+}
+class Vechicle {
+    constructor(mark, model, release_date, vin_number, reg_number, owner_info) {
+        this._mark = mark;
+        this._model = model;
+        this._release_date = release_date;
+        this._vin_number = vin_number;
+        this._reg_number = reg_number;
+        this._owner_info = owner_info;
+    }
+    getCarInfo() {
+        return 'Марка: ' + this._mark + ' Модель: ' + this._model + ' Дата выпуска: ' + this._release_date + ' Вин номер: ' + this._vin_number + ' Регистрационный номер: ' + this._reg_number;
+    }
+}
+let Danel = new Owner('Kud', 'Danel', 'Yirievch', '02.01.2005', docktype.passpart, '33333', '4444');
+console.log(Danel.getInfo());
+let Cool_Car = new Vechicle('Toyota', 'Land Cruiser Prado', '2013', 'VIN23122321313', 'E777KH777', Danel);
+console.log(Cool_Car.getCarInfo());
+var car_body;
+(function (car_body) {
+    car_body["hatchback"] = "\u0425\u0435\u0442\u0447\u0431\u0435\u043A";
+    car_body["universal"] = "\u0423\u043D\u0438\u0432\u0435\u0440\u0441\u0430\u043B";
+    car_body["sedan"] = "\u0441\u0435\u0434\u0430\u043D";
+})(car_body || (car_body = {}));
 ;
-console.log('Answer:');
-console.log(types_of_oil.cocoa_butter);
-class Pet {
-    constructor() {
-        this.name = 'Some pet';
-        this.age = -1;
+var car_class;
+(function (car_class) {
+    car_class["bisnes"] = "\u0431\u0438\u0437\u043D\u0435\u0441";
+    car_class["comfort"] = "\u043A\u043E\u043C\u0444\u043E\u0440\u0442";
+    car_class["econom"] = "\u044D\u043A\u043E\u043D\u043E\u043C";
+})(car_class || (car_class = {}));
+;
+class Car extends Vechicle {
+    constructor(mark, model, release_date, vin_number, reg_number, owner_info, car_body, car_class) {
+        super(mark, model, release_date, vin_number, reg_number, owner_info);
+        this._car_body = car_body;
+        this._car_class = car_class;
     }
-    speak() {
-        return "No speak. I am fish!";
-    }
-}
-class Dog extends Pet {
-    constructor() {
-        super(...arguments);
-        this.label = "AngryHunter";
-        this.age = 8;
-    }
-    speak() {
-        return "Yaw-Gaw!";
+    getCarInfo() {
+        return super.getCarInfo() + " Тип кузова: " + this._car_body + " Класс Автомобиля: " + this._car_class;
     }
 }
-class Cat extends Pet {
-    constructor() {
-        super(...arguments);
-        this.name = 'Barsik';
-        this.age = 2;
+let newCool_Car = new Car('Toyota', 'Camry 3.5', '2013', 'VIN23122321313', 'E777EE777', Danel, car_body.sedan, car_class.comfort);
+console.log(newCool_Car.getCarInfo());
+class Motobike extends Vechicle {
+    constructor(mark, model, release_date, vin_number, reg_number, owner_info, frame_type, for_sport) {
+        super(mark, model, release_date, vin_number, reg_number, owner_info);
+        this._frame_type = frame_type;
+        this._for_sport = for_sport;
     }
-    speak() {
-        return "Miyau!";
-    }
-}
-function printPetInfo(pet) {
-    console.log(`Name: ${pet.name}`);
-    console.log(`Age: ${pet.age}`);
-    console.log(`Speak: ${pet.speak()}`);
-    if (pet instanceof Dog) {
-        console.log(`Label: ${pet.label}`);
+    getCarInfo() {
+        return super.getCarInfo() + " Тип рамы: " + this._frame_type + " Для спорта: " + this._for_sport;
     }
 }
-const myDog = new Dog();
-const myCat = new Cat();
-console.log('4:');
-console.log('Answer:');
-printPetInfo(myDog);
-printPetInfo(myCat);
-console.log('5:');
-let myOil = {
-    Proteins: 0,
-    Fats: 99,
-    Carbohydrates: 0,
-    type: types_of_oil.soybean_oil
-};
-console.log('Answer:');
-console.log(JSON.stringify(myOil));
+let Cool_Motobile = new Motobike('Yamaha', 'R1', '2007', 'VIN231434221313', 'H777HH777', Danel, "Монокок", true);
+console.log(Cool_Motobile.getCarInfo());
+class VechicleStorage {
+    constructor(all_Vehicles, creation_data) {
+        this._all_Vehicles = all_Vehicles;
+        this._creation_data = creation_data;
+    }
+    getAllVehicle() {
+        return this._all_Vehicles;
+    }
+}
+let arr_veh = [Cool_Car, newCool_Car, Cool_Motobile];
+let Veh_Storage = new VechicleStorage(arr_veh, '30.03.2005');
+console.log(Veh_Storage.getAllVehicle());
--- main/main.ts
+++ main/main.ts
@@ -1,110 +1,145 @@
-import { TupleType } from "typescript";
-
-let array0: number[] = [2, 4, 6];
-let array1: number[] = [3, 5, 7];
-let interval: number[] = [3, 4, 5, 44];
-
-let f1 = (arr:number[]): number =>{
-    let sum:number = 0;
-    for(let num of arr){
-        sum += num;
-    }
-    return sum;
-}
-console.log('1:');
-console.log('Array:',array0);
-console.log('Answer:');
-console.log(f1(array0));
-
-
-let array2: number[][] = [array0, array1];
-console.log(array2);
-console.log('interval:', interval);
-
-let f2 = (arr:number[][], interval:number[]): number =>{
-    let count:number = 0;
-    for(let num of interval){
-        if(arr[0].includes(num)){
-            count+=1;
-        }
-        if(arr[1].includes(num)){
-            count+=1;
-        }
-    }
-    return count;
-}
-console.log('Answer:');
-console.log(f2(array2, interval))
-
-
-console.log('2:');
-let tuple: [string, string, number] = ['Ivan', 'Ivanov', 52];
-let f3 = (tuple:[string, string, number]): string =>{
-    return tuple[0] + '-' + tuple[2] + '-' + tuple[1];
-}
-console.log('Tuple:', tuple);
-console.log('Answer:');
-console.log(f3(tuple));
-
-console.log('3:');
-enum types_of_oil{cocoa_butter='Cocoa butter', sesame_oil='Sesame oil', soybean_oil='Soybean oil'};
-console.log('Answer:');
-console.log(types_of_oil.cocoa_butter);
-
-
-class Pet {
-    name: string = 'Some pet'
-    age: number = -1
-    speak() {
-    return "No speak. I am fish!";
-    }
-    }
-
-    class Dog extends Pet {
-        label = "AngryHunter";
-        age = 8;
-        speak() {
-        return "Yaw-Gaw!";
-        }
-        }
-        class Cat extends Pet {
-        name = 'Barsik';
-        age = 2;
-        speak() {
-        return "Miyau!";
-        }
-    }
-
-    function printPetInfo<T extends Pet>(pet: T): void {
-        console.log(`Name: ${pet.name}`);
-        console.log(`Age: ${pet.age}`);
-        console.log(`Speak: ${pet.speak()}`);
-        
-        if (pet instanceof Dog) {
-            console.log(`Label: ${pet.label}`);
-        }
-    }
-    
-    const myDog = new Dog();
-    const myCat = new Cat();
-    console.log('4:');
-    console.log('Answer:');
-    printPetInfo(myDog);
-    printPetInfo(myCat);
-
-console.log('5:');
-type PFC = {
-    Proteins: number;
-    Fats: number;
-    Carbohydrates: number;
-    type: types_of_oil;
+enum docktype{passpart='Пасспорт', snils='СНИЛС'}
+interface Owner{
+    surname:string;
+    name:string;
+    subname:string;
+    birthday:string;
+    docktype: docktype;
+    dock_series: string;
+    dock_number: string;
+    getInfo(): string;
 }
 
-let myOil: PFC = {
-    Proteins: 0,
-    Fats: 99,
-    Carbohydrates: 0,
-    type: types_of_oil.soybean_oil
+interface Vechicle{
+    mark:string;
+    model:string;
+    release_date:string;
+    vin_number:string;
+    reg_number:string;
+    owner_info:Owner;
+    getCarInfo():string;
 }
-console.log('Answer:');
-console.log(JSON.stringify(myOil));
+
+class Owner implements Owner{
+    private _surname:string;
+    private _name:string;
+    private _subname:string;
+    private _birthday:string;
+    private _docktype: docktype;
+    private _dock_series: string;
+    private _dock_number: string;
+
+    getSurname(): string{
+        return this._surname;
+    }
+    getName(): string{
+        return this._name;
+    }
+    constructor(surname:string, name:string, subname:string, birthday:string, docktype:docktype, dock_series:string, dock_number:string){
+        this._surname = surname;
+        this._name = name;
+        this._subname = subname;
+        this._birthday = birthday;
+        this._docktype = docktype;
+        this._dock_series = dock_series;
+        this._dock_number = dock_number;
+    }
+
+    getInfo():string{
+        return 'Фамилия: ' + this._surname + ' Имя: ' +  this._name + ' Отчество: ' +  this._subname + ' Дата рождения: ' +  this._birthday + ' Тип документа: ' +  this._docktype + ' Серия: ' +  this._dock_series + ' Номер: ' +  this._dock_number;
+    }
+}
+class Vechicle implements Vechicle{
+    private _mark:string;
+    private _model:string;
+    private _release_date:string;
+    private _vin_number:string;
+    private _reg_number:string;
+    private _owner_info:Owner;
+    constructor(mark:string, model:string, release_date:string, vin_number:string, reg_number:string, owner_info:Owner){
+        this._mark = mark;
+        this._model = model;
+        this._release_date = release_date;
+        this._vin_number = vin_number;
+        this._reg_number = reg_number;
+        this._owner_info = owner_info; 
+    }
+    getCarInfo():string{
+        return 'Марка: ' + this._mark + ' Модель: ' + this._model + ' Дата выпуска: '  + this._release_date + ' Вин номер: ' + this._vin_number + ' Регистрационный номер: ' + this._reg_number;
+    }
+}
+
+let Danel:Owner = new Owner('Kud', 'Danel', 'Yirievch', '02.01.2005', docktype.passpart, '33333', '4444');
+console.log(Danel.getInfo());
+let Cool_Car:Vechicle = new Vechicle('Toyota', 'Land Cruiser Prado', '2013', 'VIN23122321313', 'E777KH777', Danel);
+console.log(Cool_Car.getCarInfo());
+
+
+enum car_body{hatchback='Хетчбек', universal='Универсал', sedan = 'седан'};
+enum car_class{bisnes='бизнес', comfort='комфорт', econom='эконом'};
+interface Car extends Vechicle{
+    car_body:car_body;
+    car_class:car_class;
+}
+
+class Car extends Vechicle implements Car{
+    private _car_body:car_body;
+    private _car_class:car_class;
+    constructor(mark:string, model:string, release_date:string, vin_number:string, reg_number:string, owner_info:Owner, car_body:car_body, car_class:car_class){
+        super(mark, model, release_date, vin_number, reg_number, owner_info);
+        this._car_body = car_body;
+        this._car_class = car_class;
+    }
+    getCarInfo(): string {
+        return super.getCarInfo() + " Тип кузова: " + this._car_body + " Класс Автомобиля: " + this._car_class;
+    }
+}
+
+let newCool_Car: Car = new Car('Toyota', 'Camry 3.5', '2013', 'VIN23122321313', 'E777EE777', Danel, car_body.sedan, car_class.comfort);
+console.log(newCool_Car.getCarInfo());
+
+
+interface Motobike extends Vechicle{
+    frame_type:string;
+    for_sport:boolean;
+}
+
+class Motobike extends Vechicle implements Motobike{
+    private _frame_type:string;
+    private _for_sport:boolean;
+    constructor(mark:string, model:string, release_date:string, vin_number:string, reg_number:string, owner_info:Owner, frame_type:string, for_sport:boolean){
+        super(mark, model, release_date, vin_number, reg_number, owner_info);
+        this._frame_type = frame_type;
+        this._for_sport = for_sport;
+    }
+    getCarInfo(): string {
+        return super.getCarInfo() + " Тип рамы: " + this._frame_type + " Для спорта: " + this._for_sport;
+    }
+}
+
+let Cool_Motobile: Motobike = new Motobike('Yamaha', 'R1', '2007', 'VIN231434221313', 'H777HH777', Danel, "Монокок", true);
+console.log(Cool_Motobile.getCarInfo());
+
+interface VechicleStorage<T extends Vechicle>{
+    creation_data:string;
+    all_Vehicles:Array<T>;
+    getAllVehicle(): Array<T>;
+}
+
+class VechicleStorage<T extends Vechicle> implements VechicleStorage<T> {
+    private _creation_data: string;
+    private _all_Vehicles: Array<T>;
+
+    constructor(all_Vehicles: Array<T>, creation_data: string) {
+        this._all_Vehicles = all_Vehicles;
+        this._creation_data = creation_data;
+    }
+
+    getAllVehicle(): Array<T> {
+        return this._all_Vehicles;
+    }
+
+}
+let arr_veh: Vechicle[] = [Cool_Car, newCool_Car, Cool_Motobile];
+let Veh_Storage:VechicleStorage<Vechicle> = new VechicleStorage(arr_veh, '30.03.2005');
+console.log(Veh_Storage.getAllVehicle());
